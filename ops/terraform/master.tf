4eca5ee79194edc3da94585a7e14c1591518c73a
Nils Quirmbach <nils.quirmach@outlook.com>
2024-10-03 16:11:57 +0200

    fix typo

--- ops/terraform/master.tf
+++ ops/terraform/master.tf
@@ -21,7 +21,7 @@ resource "azurerm_network_interface" "master_nic" {
 }
 resource "azurerm_network_interface_security_group_association" "master_nic_nsg" {
   network_interface_id      = azurerm_network_interface.master_nic.id
-  network_security_group_id = azurerm_network_security_group.nsg
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "master" {
