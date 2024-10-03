e0eea5b86ba65d0ffa36e09b2c8e8d279a950356
Nils Quirmbach <nils.quirmach@outlook.com>
2024-10-03 18:40:40 +0200

    Open required ports on master node

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,24 +48,3 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 
 }
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "k8s-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = local.tags
-}
-
-resource "azurerm_network_security_rule" "ssh_rule" {
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-  name                        = "SSH"
-  priority                    = 1001
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-}
--- terraform/master.tf
+++ terraform/master.tf
@@ -1,3 +1,61 @@
+locals {
+  master_node_network_rules = {
+    "22" = {
+      name      = "SSH"
+      priority  = 1001
+      direction = "Inbound"
+    }
+    "6443" = {
+      name      = "Kubernetes API server"
+      priority  = 2001
+      direction = "Inbound"
+    }
+    "2379-2380" = {
+      name      = "etcd server client API"
+      priority  = 2002
+      direction = "Inbound"
+    }
+    "10250" = {
+      name      = "Kubelet API"
+      priority  = 2003
+      direction = "Inbound"
+    }
+    "10259" = {
+      name      = "kube-scheduler"
+      priority  = 2004
+      direction = "Inbound"
+    }
+    "10257" = {
+      name      = "kube-controller-manager"
+      priority  = 2005
+      direction = "Inbound"
+    }
+  }
+}
+
+resource "azurerm_network_security_group" "master_nsg" {
+  name                = "k8s-cluster-master-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+}
+
+resource "azurerm_network_security_rule" "master_nsg_rules" {
+  for_each = local.master_node_network_rules
+
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.master_nsg.name
+  destination_port_range      = each.key
+  name                        = each.value.name
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
+
 resource "azurerm_public_ip" "master_public_ip" {
   name                = "k8s-cluster-master-ip"
   location            = azurerm_resource_group.rg.location
@@ -21,7 +79,7 @@ resource "azurerm_network_interface" "master_nic" {
 }
 resource "azurerm_network_interface_security_group_association" "master_nic_nsg" {
   network_interface_id      = azurerm_network_interface.master_nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+  network_security_group_id = azurerm_network_security_group.master_nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "master" {
